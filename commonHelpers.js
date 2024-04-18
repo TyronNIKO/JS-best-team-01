c7d61bf32f341ff1ec9a042acd94f27a3e707dd1
TyronNIKO <33459109+TyronNIKO@users.noreply.github.com>
2024-04-18 17:23:50 +0000

    Deploying to gh-pages from @ TyronNIKO/JS-best-team-01@f79d3e2702865beea813a3b78152e83d680ce68b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,13 @@
-import{S as n,N as c,P as l,A as a}from"./assets/vendor-544adac3.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const o of t.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();new n(".swiper",{modules:[c,l,a],slidesPerView:1,spaceBetween:0,centeredSlides:!0,loop:!0,autoplay:{delay:3e3,disableOnInteraction:!0},pagination:{el:".swiper-pagination",clickable:!0}});
+import{S as n,N as l,P as d,A as u}from"./assets/vendor-544adac3.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const c of r.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&a(c)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();new n(".swiper",{modules:[l,d,u],slidesPerView:1,spaceBetween:0,centeredSlides:!0,loop:!0,autoplay:{delay:3e3,disableOnInteraction:!0},pagination:{el:".swiper-pagination",clickable:!0}});let o;const p=()=>{let s=document.querySelector(".catalog-list");for(let t=0;t<o.length;t++){let i=`
+    <li class="catalog-list-item">
+        <div class="product-card product-id-${o[t].id}">
+            <h3 class="product-card-title">${o[t].name}</h3>
+            <p class="product-card-description">${o[t].description}</p>
+            <img class="product-card-img" src="${o[t].img}" alt="${o[t].name}">
+            <p class="product-card-price">${o[t].curency}${o[t].price}</p>
+            <p class="product-card-compare">Compare</p>
+            <button class="product-card-btn" type="button">Order</button>
+        </div>
+    </li>
+    `;s.insertAdjacentHTML("afterbegin",i)}};p();
 //# sourceMappingURL=commonHelpers.js.map
